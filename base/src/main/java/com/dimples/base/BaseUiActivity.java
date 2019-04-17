863258175d78e95dc8a4f650ae88d1dba1d21480
dimples822 <1126834403@qq.com>
2019-04-17 20:34:06 +0800

    新建base文件

---
+++ base/src/main/java/com/dimples/base/BaseUiActivity.java
@@ -0,0 +1,27 @@
+package com.dimples.base;
+
+/**
+ * UI相关的Activity基类
+ *
+ * @author zhongyj
+ * @date 2019/4/17 20:12
+ */
+public abstract class BaseUiActivity {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
