8b25ce9e20cdaa9a74b543c542004f405f447839
dimples822 <1126834403@qq.com>
2019-04-29 21:51:49 +0800

    添加application类

---
+++ app/src/main/java/com/dimples/framework/base/MyApplication.java
@@ -0,0 +1,11 @@
+package com.dimples.framework.base;
+
+import android.app.Application;
+
+/**
+  *
+  * @author zhongyj
+  * @date 2019/4/29
+  */
+public class MyApplication extends Application {
+}
